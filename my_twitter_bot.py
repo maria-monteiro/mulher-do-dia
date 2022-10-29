13a9ad7c7cfc1d13022bc6114e0013403aafd213
Maria <mariarochamc9@gmail.com>
2022-10-29 10:53:10 -0300

    add file

--- my_twitter_bot.py
+++ my_twitter_bot.py
@@ -6,10 +6,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 client = tweepy.Client(os.getenv('BEARER_TOKEN'))
-user = client.get_user(username='ashra43')
+user = client.get_user(username='MulherDoDia')
 tweets = client.get_users_tweets(user.data.id)
-for data in tweets.data:
-    print(data.text)
+mentions = client.get_users_mentions(user.data.id)
+for data in mentions.data:
+    # print(str(data.id) + data.text)
+    if '@MulherDoDia'.lower() in data.text.lower():
+        print(data.text)
+
+
+
+
+
+
 
 
 
